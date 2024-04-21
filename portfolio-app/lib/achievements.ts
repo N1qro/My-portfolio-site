ff512cc503ef1b2cdab7f9e1986d7b0d5157ccad
Andrew <andrey777-novikov777@yandex.ru>
2024-04-21 22:11:01 +0300

    Achievements refactor | Added documents

--- portfolio-app/lib/achievements.ts
+++ portfolio-app/lib/achievements.ts
@@ -1,3 +1,5 @@
+import predprofParticipate from "@/assets/img/predprof_participate.png"
+
 interface Achievement {
   text: string,
   url?: string,
@@ -7,41 +9,49 @@ interface Achievement {
 
 const achievements: Achievement[] = [
   {
-    text: "Закончил курс \"Основы программирования на Python\" с рейтингом в 70+ баллов",
-    acquiredYear: 2022,
-    url: "yea",
-  },
-  {
-    text: "Закончил курс \"Промышленное программирование на Python\" с рейтингом в 100+ баллов",
-    acquiredYear: 2023,
-  },
-  {
-    text: "Закончил курс Advanced React для разработки Frontend приложений",
-    acquiredYear: 2023,
-  },
-  {
-    text: "Окончил специализацию Яндекса по программе \"Web-разработка на Django\"",
-    acquiredYear: 2024,
-  },
-  {
-    text: "Финалист предпрофессиональной олимпиады школьников по профилю \"Информационные технологии\"",
-    acquiredYear: 2023
-  },
-  {
-    text: "Финалист хакатона \"STANKIN\Data Science. Большие данные\"",
-    acquiredYear: 2022
-  },
-  {
-    text: "Финалист и первопроходец олимпиады PROD по промышленному программированию от Тинькофф",
-    acquiredYear: 2024,
-  },
-  {
     text: "Победитель Московской предпроффесиональной олимпиады по продуктовому \"IT\" профилю 2024",
     acquiredYear: 2024,
+    url: "/docs/predprof_win.pdf",
   },
   {
     text: "Призёр 2 степени олимпиады \"РосАтом\" по информатике 2024",
     acquiredYear: 2024,
+    url: "/docs/rosatom_win.pdf"
+  },
+  {
+    text: "Закончил курс \"Основы программирования на Python\" с рейтингом в 70+ баллов",
+    acquiredYear: 2022,
+    url: "/docs/yandex_1.pdf",
+  },
+  {
+    text: "Закончил курс \"Промышленное программирование на Python\" с рейтингом в 100+ баллов",
+    acquiredYear: 2023,
+    url: "https://lms.yandex.ru/certificate/check/?certNumber=230242600&lastName=Новиков",
+  },
+  {
+    text: "Закончил курс Advanced React для разработки Frontend приложений",
+    acquiredYear: 2023,
+    url: "/docs/advanced_react.pdf",
+  },
+  {
+    text: "Окончил специализацию Яндекса по программе \"Web-разработка на Django\"",
+    acquiredYear: 2024,
+    url: "/docs/yandex_django.pdf",
+  },
+  {
+    text: "Финалист предпрофессиональной олимпиады школьников по профилю \"Информационные технологии\"",
+    acquiredYear: 2023,
+    url: predprofParticipate.src,
+  },
+  {
+    text: "Финалист хакатона \"STANKIN\Data Science. Большие данные\"",
+    acquiredYear: 2022,
+    url: "/docs/stankin.pdf"
+  },
+  {
+    text: "Финалист и первопроходец олимпиады PROD по промышленному программированию от Тинькофф",
+    acquiredYear: 2024,
+    url: "/docs/prod.pdf",
   },
 ]
 
